945e964c27d0fbd60eab1833b5bde84535ec25d2
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-05-15 16:35:02 +0530

    Update Sitemap for Pages.

--- src/pages/sitemap/page_sitemap1.xml.js
+++ src/pages/sitemap/page_sitemap1.xml.js
@@ -1,5 +1,5 @@
 import { frontendUrl } from "@/utils/variables";
-import * as fs from "fs";
+
 const Sitemap = () => {
   return null;
 };
@@ -7,29 +7,53 @@ const Sitemap = () => {
 export const getServerSideProps = async ({ res }) => {
   const BASE_URL = frontendUrl;
 
-  const staticPaths = fs
-    .readdirSync("./src/pages")
-    .filter((staticPage) => {
-      return ![
-        "api",
-        "_app.js",
-        "_document.js",
-        "sitemap.xml.js",
-        "sitemap",
-        "blog",
-        "category",
-        "[slug].js",
-        "index.js",
-        "web-stories",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      const pageName = staticPagePath.replace(/\.js$/, "");
-      return `${BASE_URL}/${pageName}`;
-    });
+  // Static paths manually defined
+  const staticPaths = [
+    "about-us",
+    "anti-slavery-and-human-trafficking-statement",
+    "become-a-partner",
+    "blog",
+    "case-studies",
+    "contact-us",
+    "cookie-policy",
+    "ebooks",
+    "email-signature-management",
+    "features",
+    "google-contact-sharing",
+    "google-drive-compliance",
+    "google-drive-management",
+    "google-workspace-backup",
+    "guides",
+    "legal",
+    "on-boarding",
+    "organisational-chart",
+    "partner-with-us",
+    "patronum-for-business",
+    "patronum-for-education",
+    "patronum-for-hr",
+    "patronum-for-it-admins",
+    "patronum-for-sales-marketing",
+    "patronum-for-users",
+    "pricing",
+    "privacy-policy",
+    "product-videos",
+    "refer-a-customer",
+    "register-an-opportunity",
+    "support",
+    "use-cases",
+    "webinars"
+  ].map(page => `${BASE_URL}/${page}`);
 
-  const mainPaths = [`${BASE_URL}/`]  
-  const redirectPaths = [`${BASE_URL}/standard-contractual-clauses`, `${BASE_URL}/partner-agreement`, `${BASE_URL}/terms-and-conditions`, `${BASE_URL}/terms-and-conditions-for-direct-customers`, `${BASE_URL}/master-subscription-agreement`, `${BASE_URL}/data-processing-agreement`, `${BASE_URL}/bespinlabs-legal-agreements-faqs`];
+  const mainPaths = [`${BASE_URL}/`];
+  const redirectPaths = [
+    `${BASE_URL}/standard-contractual-clauses`,
+    `${BASE_URL}/partner-agreement`,
+    `${BASE_URL}/terms-and-conditions`,
+    `${BASE_URL}/terms-and-conditions-for-direct-customers`,
+    `${BASE_URL}/master-subscription-agreement`,
+    `${BASE_URL}/data-processing-agreement`,
+    `${BASE_URL}/bespinlabs-legal-agreements-faqs`,
+  ];
 
   const allPaths = [...mainPaths, ...staticPaths, ...redirectPaths];
 
