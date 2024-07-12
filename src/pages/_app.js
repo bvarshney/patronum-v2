65d525e1f89645f12eecb6212a59afc35a064fa4
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-07-12 11:21:52 +0530

    Update Google Tag Manager And Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,122 +1,124 @@
-  import '@/styles/globals.css'
-  import { DefaultSeo } from 'next-seo';
-  import { SpeedInsights } from '@vercel/speed-insights/next';
-  import { Analytics } from "@vercel/analytics/react"
-  import { ReactLenis } from '@studio-freight/react-lenis';
-  import { useEffect, useState } from 'react';
-  import { AnimatePresence } from 'framer-motion';
-  import Head from 'next/head';
-  import Script from 'next/script';
-  import dynamic from 'next/dynamic'
-  import { ModalProvider } from '@/components/InstallModal/ModelContext';
-  import PreLoader from '@/components/PreLoader';
-  import DemoModal from '@/components/InstallModal/DemoModal';
-  import Pixi from '@/components/Pixi';
-  import Cookie from '@/components/Cookie';
+import '@/styles/globals.css'
+import { DefaultSeo } from 'next-seo';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
+import { ReactLenis } from '@studio-freight/react-lenis';
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Head from 'next/head';
+import Script from 'next/script';
+import dynamic from 'next/dynamic'
+import { ModalProvider } from '@/components/InstallModal/ModelContext';
+import PreLoader from '@/components/PreLoader';
+import DemoModal from '@/components/InstallModal/DemoModal';
+import Pixi from '@/components/Pixi';
+import Cookie from '@/components/Cookie';
+import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-  const InstallModalWithNoSSR = dynamic(
-    () => import('@/components/InstallModal'),
-    { ssr: false }
-  )
+const InstallModalWithNoSSR = dynamic(
+  () => import('@/components/InstallModal'),
+  { ssr: false }
+)
 
-  export default function App({ Component, pageProps, router }) {
-    const [showPreloader, setShowPreloader] = useState(true);
+export default function App({ Component, pageProps, router }) {
+  const [showPreloader, setShowPreloader] = useState(true);
 
-    useEffect(() => {
-      const hasVisited = sessionStorage.getItem('hasVisited');
+  useEffect(() => {
+    const hasVisited = sessionStorage.getItem('hasVisited');
 
-      if (!hasVisited) {
-        setShowPreloader(true); 
-        
-        const preloaderTimeout = setTimeout(() => {
-          setShowPreloader(false); 
-          sessionStorage.setItem('hasVisited', 'true');
-        }, 4000);
+    if (!hasVisited) {
+      setShowPreloader(true);
 
-        return () => clearTimeout(preloaderTimeout);
-      }
-      else {
+      const preloaderTimeout = setTimeout(() => {
         setShowPreloader(false);
-      }
-    }, []);
+        sessionStorage.setItem('hasVisited', 'true');
+      }, 4000);
 
-    useEffect(() => {
-      const handleRouteChange = () => {
-        // Disable pointer events on route change
-        document.body.style.pointerEvents = 'none';
+      return () => clearTimeout(preloaderTimeout);
+    }
+    else {
+      setShowPreloader(false);
+    }
+  }, []);
 
-        // Enable pointer events when mouse moves
-        const enablePointerEvents = () => {
-          document.body.style.pointerEvents = 'auto';
-          document.removeEventListener('mousemove', enablePointerEvents);
-        };
-        document.addEventListener('mousemove', enablePointerEvents);
+  useEffect(() => {
+    const handleRouteChange = () => {
+      // Disable pointer events on route change
+      document.body.style.pointerEvents = 'none';
+
+      // Enable pointer events when mouse moves
+      const enablePointerEvents = () => {
+        document.body.style.pointerEvents = 'auto';
+        document.removeEventListener('mousemove', enablePointerEvents);
       };
+      document.addEventListener('mousemove', enablePointerEvents);
+    };
 
-      router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeStart', handleRouteChange);
 
-      return () => {
-        router.events.off('routeChangeStart', handleRouteChange);
-      };
-    }, [router]);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [router]);
 
-      useEffect(() => {
-      const handleRouteChange = () => {
-          window.scrollTo(0, 0)
-      };
+  useEffect(() => {
+    const handleRouteChange = () => {
+      window.scrollTo(0, 0)
+    };
 
-      window.addEventListener("beforeunload", handleRouteChange);
+    window.addEventListener("beforeunload", handleRouteChange);
 
-      return () => {
-        window.removeEventListener("beforeunload", handleRouteChange);
-      };
-    }, []);
+    return () => {
+      window.removeEventListener("beforeunload", handleRouteChange);
+    };
+  }, []);
 
-    return(
-      <>
-        <DefaultSeo
-          title='Patronum - Best Platform for Google Workspace (GSuite) Management'
-          description='Patronum provides a better way to Google Workspace (GSuite) Management. Patronum fully automates all the administrator and user tasks to ensure an efficient, effective and secure process.'
-          additionalMetaTags={[
+  return (
+    <>
+      <DefaultSeo
+        title='Patronum - Best Platform for Google Workspace (GSuite) Management'
+        description='Patronum provides a better way to Google Workspace (GSuite) Management. Patronum fully automates all the administrator and user tasks to ensure an efficient, effective and secure process.'
+        additionalMetaTags={[
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1.0, maximum-scale=5.0'
+          },
+        ]}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: '/favicon.png',
+          },
+          {
+            rel: 'preload',
+            href: '/assets/fonts/Aeonik/Aeonik-Regular.woff2',
+            as: 'font',
+            type: 'font/woff2',
+            crossOrigin: 'anonymous'
+          }
+        ]}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          title: "Patronum - Best Platform for Google Workspace (GSuite) Management",
+          "description": "Patronum provides a better way to Google Workspace (GSuite) Management. Patronum fully automates all the administrator and user tasks to ensure an efficient, effective and secure process.",
+          images: [
             {
-              name: 'viewport',
-              content: 'width=device-width, initial-scale=1.0, maximum-scale=5.0'
+              url: "https://www.patronum.io/assets/seo/Google-Workspace.png",
+              width: 1290,
+              height: 594,
+              alt: "Patronum",
+              type: "image/png",
             },
-          ]}
-          additionalLinkTags={[
-            {
-              rel: 'icon',
-              href: '/favicon.png',
-            },
-            {
-              rel: 'preload',
-              href: '/assets/fonts/Aeonik/Aeonik-Regular.woff2',
-              as: 'font',
-              type: 'font/woff2',
-              crossOrigin: 'anonymous'
-            }
-          ]}
-          openGraph={{
-            type: 'website',
-            locale: 'en_US',
-            title: "Patronum - Best Platform for Google Workspace (GSuite) Management",
-            "description": "Patronum provides a better way to Google Workspace (GSuite) Management. Patronum fully automates all the administrator and user tasks to ensure an efficient, effective and secure process.",
-            images: [
-              {
-                url: "https://www.patronum.io/assets/seo/Google-Workspace.png",
-                width: 1290,
-                height: 594,
-                alt: "Patronum",
-                type: "image/png",
-              },
-            ],
-            siteName: "Patronum",
-          }}
-          twitter={{
-            site: 'Patronum',
-            cardType: 'summary_large_image',
-          }}
-        />
+          ],
+          siteName: "Patronum",
+        }}
+        twitter={{
+          site: 'Patronum',
+          cardType: 'summary_large_image',
+        }}
+      />
 
       <Head>
         <meta charSet="utf-8" />
@@ -152,7 +154,7 @@
                 "@id": "https://www.patronum.io/#website",
                 "name": "Patronum",
                 "url": "https://www.patronum.io",
-                "publisher":[
+                "publisher": [
                   {
                     "@id": "https://www.patronum.io/#organization"
                   }
@@ -181,44 +183,39 @@
         />
       </Head>
 
-        {showPreloader && <PreLoader />} 
-        <Cookie />
-        <ReactLenis root options={{ duration: 0.8 }}>
-          <ModalProvider>
-            <AnimatePresence mode="wait">
-              <Component {...pageProps} key={router.route}/>
-            </AnimatePresence>
-            <InstallModalWithNoSSR />
-            <DemoModal />
-          </ModalProvider>
-        </ReactLenis>
+      {showPreloader && <PreLoader />}
+      <Cookie />
+      <ReactLenis root options={{ duration: 0.8 }}>
+        <ModalProvider>
+          <AnimatePresence mode="wait">
+            <GoogleTagManager gtmId="GTM-NB75SD4H" />
+            <Component {...pageProps} key={router.route} />
+            <GoogleAnalytics gaId="G-QTG00X44EP" />
+          </AnimatePresence>
+          <InstallModalWithNoSSR />
+          <DemoModal />
+        </ModalProvider>
+      </ReactLenis>
 
-        {/* Vercel Analytics */}
-        <SpeedInsights />
-        <Analytics />
+      {/* Vercel Analytics */}
+      <SpeedInsights />
+      <Analytics />
 
-        {/* Google Tag Manager */}
-        <Script
-          async
-          strategy="worker"
-          src="https://www.googletagmanager.com/gtag/js?id=G-QTG00X44EP"
-        />
-
-        {/* Google Analytics */}
-        <Script
-          strategy="worker"
-          id="google-analytics"
-          >
-            {` window.dataLayer = window.dataLayer || [];
+      {/* Google Analytics */}
+      {/* <Script
+        strategy="worker"
+        id="google-analytics"
+      >
+        {` window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-QTG00X44EP', {
                 page_path: window.location.pathname,
               });
             `}
-        </Script>
-        {/* WEBGL Background */}
-        <Pixi />
-      </>
-    ); 
-  }
+      </Script> */}
+      {/* WEBGL Background */}
+      <Pixi />
+    </>
+  );
+}
