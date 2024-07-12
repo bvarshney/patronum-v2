93d9d354d6481ce66bf9c31030559e794ead760d
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-07-12 19:33:09 +0530

    Update Google Tag Manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -75,37 +75,28 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
+
+
       {/* Google Analytics */}
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-QTG00X44EP`}
-      />
-      <Script
-        strategy="lazyOnload"
-        id="google-analytics"
-      >
-        {` window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-QTG00X44EP', {
-                page_path: window.location.pathname,
-              });
-            `}
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-QTG00X44EP`}></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QTG00X44EP');
+        `}
       </Script>
-      {/* Google Analytics */}
-      {/*Google Tag Manager */}
-      {/* <Script
-        strategy="worker"
-        id="google-tag-manager"
-      >
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NB75SD4H');
-        `}
+      {/* <Script id='google-analytics' strategy='lazyOnload'>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QTG00X44EP', {
+          page_path: window.location.pathname,
+          });
+        `}
       </Script> */}
-      {/*Google Tag Manager */}
+      {/* Google Analytics */}
+
 
       <DefaultSeo
         title='Patronum - Best Platform for Google Workspace (GSuite) Management'
