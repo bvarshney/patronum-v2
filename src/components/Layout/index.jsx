9939f26738272617826e4666373df168066823a3
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-11-20 19:39:19 +0530

    Update - Background

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,4 +1,4 @@
-import { useDevice } from "@/utils/useDevice";
+// import { useDevice } from "@/utils/useDevice";
 import Transition from "../Transition";
 import dynamic from "next/dynamic";
 import { ModalProvider } from "../Modals/ModalContext";
@@ -6,13 +6,13 @@ import Header from "../Header";
 import PreLoader from "../PreLoader";
 import { useEffect, useState } from "react";
 
-const Background = dynamic(() => import("../Background"), { ssr: false });
+// const Background = dynamic(() => import("../Background"), { ssr: false });
 const Footer = dynamic(() => import("../Footer"), { ssr: false });
 const DemoModal = dynamic(() => import("@/components/Modals/DemoModal"), { ssr: false });
 const InstallModal = dynamic(() => import("@/components/Modals/InstallModal"), { ssr: false });
 
 const Layout = ({ children }) => {
-    const { isDesktop } = useDevice();
+    // const { isDesktop } = useDevice();
     const [showPreloader, setShowPreloader] = useState(true);
 
     useEffect(() => {
@@ -40,7 +40,7 @@ const Layout = ({ children }) => {
                 <InstallModal />
             </ModalProvider>
             <Transition />
-            {isDesktop && <Background />}
+            {/* {isDesktop && <Background />} */}
         </>
     );
 };
