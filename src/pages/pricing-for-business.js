247745ef33b187bd55c60cc0ec917c96bcb5e890
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2025-04-22 12:02:18 +0530

    Update

--- src/pages/pricing-for-business.js
+++ src/pages/pricing-for-business.js
@@ -17,7 +17,7 @@ export default function BusinessPricingPage() {
                 imgSrc="business-price-hero.svg"
                 title={"Pricing for Business: A Google Workspace Manager"}
                 description={"Patronum provides a better way to manage Google Workspace (G Suite). It fully automates all administrator and user tasks to ensure an efficient, effective, and secure process. For pricing information, please visit our website."}
-                url={"pricing"}
+                url={"pricing-for-business"}
                 img={"pricing.jpg"}
                 date_published={"2023-04-24T12:00"}
                 date_modified={"2024-04-04T12:00"}
--- src/pages/pricing-for-education.js
+++ src/pages/pricing-for-education.js
@@ -10,12 +10,12 @@ export default function EducationPricingPage() {
         <>
             <PageLayout
                  pageTitle1="Google Workspace "
-                pageTitle2="Management for Education"
+                pageTitle2="Management for Education & Non-Profit"
                 pagePara="Honest, Transparent and Fair Pricing."
                 imgSrc="education-price-hero.svg"
                 title={"Pricing for Education & Non-Profit: A Google Workspace Manager"}
                 description={"Patronum provides a better way to manage Google Workspace (G Suite). It fully automates all administrator and user tasks to ensure an efficient, effective, and secure process. For pricing information, please visit our website."}
-                url={"pricing"}
+                url={"pricing-for-education"}
                 img={"pricing.jpg"}
                 date_published={"2023-04-24T12:00"}
                 date_modified={"2024-04-04T12:00"}
